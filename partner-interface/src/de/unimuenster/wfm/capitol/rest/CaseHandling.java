f7e909e37a336411efaa179fcb25a5f7bc60d5e5
Julian <Julian@Julian>
2016-05-11 23:40:51 +0200

    proof of concept. Casehandling-rest instantiates the example process "invoice" from camunda.

--- partner-interface/src/de/unimuenster/wfm/capitol/rest/CaseHandling.java
+++ partner-interface/src/de/unimuenster/wfm/capitol/rest/CaseHandling.java
@@ -1,16 +1,37 @@
 package de.unimuenster.wfm.capitol.rest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
 import javax.ws.rs.*;
+
+import org.camunda.bpm.engine.RuntimeService;
+
 import de.unimuenster.wfm.capitol.dto.LiabilityCase;
 
 @Path( "casehandling" )
 public class CaseHandling {
-  @POST
-  @Consumes("application/json")
-  public void receiveCase(LiabilityCase liabilityCase){
-	  System.out.println(liabilityCase.getDescription());
-	  System.out.println(liabilityCase.getId());
-	  System.out.println(liabilityCase.getName());
-	  System.out.println(liabilityCase.getSum());	  
-  }
+	public static final String CALLBACK_URL = "callbackURL";
+	public static final String CALLBACK_CORRELATION_ID = "callbackCorrelationId";
+	public static final String PAYLOAD = "payload";
+	//TODO Make sure the casename is correct
+	public static final String MESSAGENAME = "CaseReceived";
+
+	@Inject
+	private RuntimeService runtimeService;
+	@POST
+	@Consumes("application/json")
+	public void receiveCase(LiabilityCase liabilityCase){
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("amount", new Double(liabilityCase.getSum()));
+		variables.put("invoiceNumber", "Fake Invoice");
+		variables.put("creditor", liabilityCase.getName());
+		variables.put("invoiceCategory", liabilityCase.getDescription());
+		variables.put("approverGroups", ArrayList.class);
+
+		runtimeService.startProcessInstanceByKey("invoice", variables );
+
+	}
 }
