a56b7e65442ab4ddf2a6e4c5f5ad194f93b3ac51
Julian <Julian@Julian>
2016-05-16 19:45:33 +0200

    Implemented MessageService(untested)

--- MessagingService/ejbModule/de/unimuenster/wfm/capitol/dto/ContractProposal.java
+++ MessagingService/ejbModule/de/unimuenster/wfm/capitol/dto/ContractProposal.java
@@ -1,5 +1,22 @@
 package de.unimuenster.wfm.capitol.dto;
 
 public class ContractProposal {
+	
+	String name;
+	String firm;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirm() {
+		return firm;
+	}
+	public void setFirm(String firm) {
+		this.firm = firm;
+	}
+	
+	
 
 }
--- MessagingService/ejbModule/de/unimuenster/wfm/capitol/service/MessageServiceImpl.java
+++ MessagingService/ejbModule/de/unimuenster/wfm/capitol/service/MessageServiceImpl.java
@@ -1,24 +1,64 @@
 package de.unimuenster.wfm.capitol.service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
 
 import de.unimuenster.wfm.capitol.dto.CaseDecision;
 import de.unimuenster.wfm.capitol.dto.ContractProposal;
 
 @Stateless
 public class MessageServiceImpl implements MessageService{
+	
 
 	@Override
 	public void sendContractProposal(ContractProposal proposal) {
-		// TODO Auto-generated method stub
-		System.out.println("Erfolg! Methode sendContractProposal wurde aufgerufen");
-		
+		//TODO Requires correct URL
+		sendJSON(proposal, "localhost:8080/proposal");
 	}
 
 	@Override
 	public void sendCaseDecision(CaseDecision decision) {
-		// TODO Auto-generated method stub
-		System.out.println("Erfolg! Methode sendCaseDecision wurde aufgerufen");
+		//TODO Requires correct URL
+		sendJSON(decision, "http://CAMUNDA-BVIS.uni-muenster.de/case");
+	}
+	
+	private void sendJSON(Object input, String urlString){
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		try {
+			URL url = new URL(urlString);
+			String json = ow.writeValueAsString(input);
+			System.out.println(json);
+			URLConnection connection = url.openConnection();
+			connection.setDoOutput(true);
+			connection.setRequestProperty("Content-Type", "application/json");
+			connection.setConnectTimeout(10000);
+			connection.setReadTimeout(10000);
+			OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream());
+			out.write(json);
+			out.close();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+			while (in.readLine() != null) {
+				System.out.println(in.toString());
+			}
+			System.out.println("REST Service Invoked Successfully..");
+			in.close();
+		} catch (Exception e) {
+			System.out.println("Error while calling  REST Service");
+			System.out.println(e);
+		}
 	}
 
 }
