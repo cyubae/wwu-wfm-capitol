623937d8db0645d38cdcf9b9f65980651dc2685d
KevinGruppe <k_grup01@uni-muenster.de>
2016-06-18 15:18:16 +0200

    Vertauscht zwischen Claim und ContractDetails :)

--- partner-interface/src/de/unimuenster/wfm/capitol/dto/contract/ContractDetailsDTO.java
+++ partner-interface/src/de/unimuenster/wfm/capitol/dto/contract/ContractDetailsDTO.java
@@ -1,5 +1,291 @@
 package de.unimuenster.wfm.capitol.dto.contract;
 
+import java.util.Calendar;
+import java.util.Collection;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class ContractDetailsDTO {
+	private String process_id;
+	private Order order;
+
+
+	public String getProcess_id() {
+		return process_id;
+	}
+	public void setProcess_id(String process_id) {
+		this.process_id = process_id;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+	
+	public class Order {
+		private int order_id;
+		private Calendar request_date;
+		private boolean fleet_rental;
+		private String inquiry_text;
+		private User user;
+		private Collection<Car> cars;
+		private Insurance insurance;
+		
+		public int getOrder_id() {
+			return order_id;
+		}
+
+		public void setOrder_id(int order_id) {
+			this.order_id = order_id;
+		}
+
+		public Calendar getRequest_date() {
+			return request_date;
+		}
+
+		public void setRequest_date(Calendar request_date) {
+			this.request_date = request_date;
+		}
+
+		public boolean isFleet_rental() {
+			return fleet_rental;
+		}
+
+		public void setFleet_rental(boolean fleet_rental) {
+			this.fleet_rental = fleet_rental;
+		}
+
+		public String getInquiry_text() {
+			return inquiry_text;
+		}
+
+		public void setInquiry_text(String inquiry_text) {
+			this.inquiry_text = inquiry_text;
+		}
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+		public Collection<Car> getCars() {
+			return cars;
+		}
+
+		public void setCars(Collection<Car> cars) {
+			this.cars = cars;
+		}
+
+		public Insurance getInsurance() {
+			return insurance;
+		}
+
+		public void setInsurance(Insurance insurance) {
+			this.insurance = insurance;
+		}
+		
+		public class User {
+			private String firstname;
+			private String surname;
+			private String email;
+			private String phone_number;
+			private String street;
+			private String house_number;
+			private String postcode;
+			private String city;
+			private String country;
+			private Calendar date_of_birth;
+			private boolean company;
+			private String company_name;
+			
+			public String getFirstname() {
+				return firstname;
+			}
+			public void setFirstname(String firstname) {
+				this.firstname = firstname;
+			}
+			public String getSurname() {
+				return surname;
+			}
+			public void setSurname(String surname) {
+				this.surname = surname;
+			}
+			public String getEmail() {
+				return email;
+			}
+			public void setEmail(String email) {
+				this.email = email;
+			}
+			public String getPhone_number() {
+				return phone_number;
+			}
+			public void setPhone_number(String phone_number) {
+				this.phone_number = phone_number;
+			}
+			public String getStreet() {
+				return street;
+			}
+			public void setStreet(String street) {
+				this.street = street;
+			}
+			public String getHouse_number() {
+				return house_number;
+			}
+			public void setHouse_number(String house_number) {
+				this.house_number = house_number;
+			}
+			public String getPostcode() {
+				return postcode;
+			}
+			public void setPostcode(String postcode) {
+				this.postcode = postcode;
+			}
+			public String getCity() {
+				return city;
+			}
+			public void setCity(String city) {
+				this.city = city;
+			}
+			public String getCountry() {
+				return country;
+			}
+			public void setCountry(String country) {
+				this.country = country;
+			}
+			public Calendar getDate_of_birth() {
+				return date_of_birth;
+			}
+			public void setDate_of_birth(Calendar date_of_birth) {
+				this.date_of_birth = date_of_birth;
+			}
+			public boolean isCompany() {
+				return company;
+			}
+			public void setCompany(boolean company) {
+				this.company = company;
+			}
+			public String getCompany_name() {
+				return company_name;
+			}
+			public void setCompany_name(String company_name) {
+				this.company_name = company_name;
+			}
+		}
+		
+		public class Car {
+			private String registration_number;
+			private String brand;
+			private String type;
+			private String model;
+			private String vehicle_identification_number;
+			private String fuel_type;
+			private int ps;
+			private int construction_year;
+			
+			public String getRegistration_number() {
+				return registration_number;
+			}
+			public void setRegistration_number(String registration_number) {
+				this.registration_number = registration_number;
+			}
+			public String getBrand() {
+				return brand;
+			}
+			public void setBrand(String brand) {
+				this.brand = brand;
+			}
+			public String getType() {
+				return type;
+			}
+			public void setType(String type) {
+				this.type = type;
+			}
+			public String getModel() {
+				return model;
+			}
+			public void setModel(String model) {
+				this.model = model;
+			}
+			public String getVehicle_identification_number() {
+				return vehicle_identification_number;
+			}
+			public void setVehicle_identification_number(String vehicle_identification_number) {
+				this.vehicle_identification_number = vehicle_identification_number;
+			}
+			public String getFuel_type() {
+				return fuel_type;
+			}
+			public void setFuel_type(String fuel_type) {
+				this.fuel_type = fuel_type;
+			}
+			public int getPs() {
+				return ps;
+			}
+			public void setPs(int ps) {
+				this.ps = ps;
+			}
+			public int getConstruction_year() {
+				return construction_year;
+			}
+			public void setConstruction_year(int construction_year) {
+				this.construction_year = construction_year;
+			}
+		}
+		
+		public class Insurance {
+			private int insurance_id;
+			private String type;
+			private double deductible;
+			private Calendar pick_up_date;
+			private Calendar return_date;
+			private double estimated_of_cost;
+			
+			public int getInsurance_id() {
+				return insurance_id;
+			}
+			public void setInsurance_id(int insurance_id) {
+				this.insurance_id = insurance_id;
+			}
+			public String getType() {
+				return type;
+			}
+			public void setType(String type) {
+				this.type = type;
+			}
+			public double getDeductible() {
+				return deductible;
+			}
+			public void setDeductible(double deductible) {
+				this.deductible = deductible;
+			}
+			public Calendar getPick_up_date() {
+				return pick_up_date;
+			}
+			public void setPick_up_date(Calendar pick_up_date) {
+				this.pick_up_date = pick_up_date;
+			}
+			public Calendar getReturn_date() {
+				return return_date;
+			}
+			public void setReturn_date(Calendar return_date) {
+				this.return_date = return_date;
+			}
+			public double getEstimated_of_cost() {
+				return estimated_of_cost;
+			}
+			public void setEstimated_of_cost(double estimated_of_cost) {
+				this.estimated_of_cost = estimated_of_cost;
+			}
+		}
+
+	}
 
 }
--- partner-interface/src/de/unimuenster/wfm/capitol/rest/contract/ContractDetails.java
+++ partner-interface/src/de/unimuenster/wfm/capitol/rest/contract/ContractDetails.java
@@ -1,5 +1,108 @@
 package de.unimuenster.wfm.capitol.rest.contract;
 
-public class ContractDetails {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.inject.Inject;
+import javax.ws.rs.*;
+
+import org.camunda.bpm.engine.MismatchingMessageCorrelationException;
+import org.camunda.bpm.engine.RuntimeService;
+
+import de.unimuenster.wfm.capitol.dto.claim.ContractDetailsDTO;
+import de.unimuenster.wfm.capitol.dto.claim.ContractDetailsDTO.Order.Car;
+
+@Path( "contractdetails" )
+
+public class ContractDetails {
+	public static final String MESSAGENAME = "contract_details";
+
+	@Inject
+	private RuntimeService runtimeService;
+	@POST
+	@Consumes("application/json")
+	public String receiveCase(ContractDetailsDTO contractdetails){
+		// TESTCODE
+		System.out.println(contractdetails.getProcess_id());
+		System.out.println(contractdetails.getOrder().getOrder_id());
+		System.out.println(contractdetails.getOrder().getRequest_date());
+		System.out.println(contractdetails.getOrder().isFleet_rental());
+		System.out.println(contractdetails.getOrder().getInquiry_text());
+		System.out.println(contractdetails.getOrder().getUser().getFirstname());
+		System.out.println(contractdetails.getOrder().getUser().getSurname());
+		System.out.println(contractdetails.getOrder().getUser().getEmail());
+		System.out.println(contractdetails.getOrder().getUser().getPhone_number());
+		System.out.println(contractdetails.getOrder().getUser().getStreet());
+		System.out.println(contractdetails.getOrder().getUser().getHouse_number());
+		System.out.println(contractdetails.getOrder().getUser().getPostcode());
+		System.out.println(contractdetails.getOrder().getUser().getCity());
+		System.out.println(contractdetails.getOrder().getUser().getCountry());
+		System.out.println(contractdetails.getOrder().getUser().getDate_of_birth());
+		System.out.println(contractdetails.getOrder().getUser().isCompany());
+		System.out.println(contractdetails.getOrder().getUser().getCompany_name());
+		for(Car single_car : contractdetails.getOrder().getCars()) {
+			System.out.println(single_car.getRegistration_number());
+			System.out.println(single_car.getBrand());
+			System.out.println(single_car.getType());
+			System.out.println(single_car.getModel());
+			System.out.println(single_car.getVehicle_identification_number());
+			System.out.println(single_car.getFuel_type());
+			System.out.println(single_car.getPs());
+			System.out.println(single_car.getConstruction_year());
+		}
+		System.out.println(contractdetails.getOrder().getInsurance().getInsurance_id());
+		System.out.println(contractdetails.getOrder().getInsurance().getType());
+		System.out.println(contractdetails.getOrder().getInsurance().getDeductible());
+		System.out.println(contractdetails.getOrder().getInsurance().getPick_up_date());
+		System.out.println(contractdetails.getOrder().getInsurance().getReturn_date());
+		System.out.println(contractdetails.getOrder().getInsurance().getEstimated_of_cost());
+		// END TESTCODE
+		
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("order_id", contractdetails.getOrder().getOrder_id());
+		variables.put("request_date", contractdetails.getOrder().getRequest_date());
+		variables.put("fleet_rental", contractdetails.getOrder().isFleet_rental());
+		variables.put("inquiry_text", contractdetails.getOrder().getInquiry_text());
+		variables.put("user_firstname", contractdetails.getOrder().getUser().getFirstname());
+		variables.put("user_surname", contractdetails.getOrder().getUser().getSurname());
+		variables.put("user_email", contractdetails.getOrder().getUser().getEmail());
+		variables.put("user_phone_number", contractdetails.getOrder().getUser().getPhone_number());
+		variables.put("user_street", contractdetails.getOrder().getUser().getStreet());
+		variables.put("user_house_number", contractdetails.getOrder().getUser().getHouse_number());
+		variables.put("user_postcode", contractdetails.getOrder().getUser().getPostcode());
+		variables.put("user_city", contractdetails.getOrder().getUser().getCity());
+		variables.put("user_country", contractdetails.getOrder().getUser().getCountry());
+		variables.put("user_date_of_birth", contractdetails.getOrder().getUser().getDate_of_birth());
+		variables.put("user_iscompany", contractdetails.getOrder().getUser().isCompany());
+		variables.put("user_company_name", contractdetails.getOrder().getUser().getCompany_name());
+		for(Car single_car : contractdetails.getOrder().getCars()) {
+			variables.put("car_registration_number", single_car.getRegistration_number());
+			variables.put("car_brand", single_car.getBrand());
+			variables.put("car_type", single_car.getType());
+			variables.put("car_model", single_car.getModel());
+			variables.put("car_vehicle_identification_number", single_car.getVehicle_identification_number());
+			variables.put("car_fuel_type", single_car.getFuel_type());
+			variables.put("car_ps", single_car.getPs());
+			variables.put("car_construction_year", single_car.getConstruction_year());
+		}
+		variables.put("insurance_insurance_id", contractdetails.getOrder().getInsurance().getInsurance_id());
+		variables.put("insurance_type", contractdetails.getOrder().getInsurance().getType());
+		variables.put("insurance_deductible", contractdetails.getOrder().getInsurance().getDeductible());
+		variables.put("insurance_pick_up_date", contractdetails.getOrder().getInsurance().getPick_up_date());
+		variables.put("insurance_return_date", contractdetails.getOrder().getInsurance().getReturn_date());
+		variables.put("insurance_estimated_of_cost", contractdetails.getOrder().getInsurance().getEstimated_of_cost());
+		
+		try {
+			// correlate the message
+			  runtimeService.createMessageCorrelation("Message-Approve")
+		      .processInstanceId(contractdetails.getProcess_id())
+		      .setVariables(variables)
+		      .correlate();
+		} catch (MismatchingMessageCorrelationException e) {
+			return "Failure";
+		}
+		
+		return "Success";
+	}
 }
