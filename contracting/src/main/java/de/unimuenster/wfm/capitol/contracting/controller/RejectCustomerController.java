e1f9276779eb5fd339471b2a5764ca3ecca744dd
cyubae <cyubae@gmail.com>
2016-06-30 22:48:48 +0200

    Add rejection check in contracting

---
+++ contracting/src/main/java/de/unimuenster/wfm/capitol/contracting/controller/RejectCustomerController.java
@@ -0,0 +1,70 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.unimuenster.wfm.capitol.contracting.controller;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.enterprise.context.ConversationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.camunda.bpm.engine.cdi.BusinessProcess;
+import org.camunda.bpm.engine.cdi.jsf.TaskForm;
+
+import de.unimuenster.wfm.capitol.entities.Customer;
+import de.unimuenster.wfm.capitol.jpa.AccessCustomer;
+
+@Named
+@ConversationScoped
+public class RejectCustomerController implements Serializable {
+
+	private static  final long serialVersionUID = 1L;
+
+	// the BusinessProcess to access the process variables
+	@Inject
+	private BusinessProcess businessProcess;
+
+	// Inject the AccessCustomer to update the persisted customer
+	@Inject
+	private AccessCustomer accessCustomer;
+
+	// Inject task form available through the camunda cdi artifact
+	@Inject
+	private TaskForm taskForm;	
+
+	// Caches the Customer during the conversation
+	private Customer customer;
+
+	public Customer getCustomer() {
+		if (customer == null) {
+			// Load the customer from the database if not already cached
+			customer = accessCustomer.getCustomer((Integer) businessProcess.getVariable("customerId"));
+		}
+		return customer;
+	}
+
+	public void submitRejection(boolean reject) throws IOException {
+		businessProcess.setVariable("user_rejected", reject);		
+		try {
+			// Complete user task from
+			taskForm.completeTask();
+		} catch (IOException e) {
+			// Rollback both transactions on error
+			throw new RuntimeException("Cannot complete task", e);
+		}
+	}
+}
+
