b0cf3aea35aba458bba383138a210ec5aae1206e
cyubae <cyubae@gmail.com>
2016-07-04 22:19:45 +0200

    Add release contract business logic

--- contracting/src/main/java/de/unimuenster/wfm/capitol/contracting/businesslogic/ReleaseContractBL.java
+++ contracting/src/main/java/de/unimuenster/wfm/capitol/contracting/businesslogic/ReleaseContractBL.java
@@ -1,23 +1,26 @@
 package de.unimuenster.wfm.capitol.contracting.businesslogic;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+
+import de.unimuenster.wfm.capitol.entities.Contract;
+import de.unimuenster.wfm.capitol.jpa.ContractCRUD;
 
 @Stateless
 @Named
 public class ReleaseContractBL {
 
-	  // Inject the entity manager
-	  @PersistenceContext
-	  private EntityManager entityManager;
-	  
-	  public void performBusinessLogic(DelegateExecution delegateExecution) {
-		  
-	  }
+	// Inject CRUD-Services to access persistence unit	
+	@Inject
+	private ContractCRUD contractCRUD;
+
+	public void performBusinessLogic(DelegateExecution delegateExecution) {
+		Contract contract  = contractCRUD.find((Integer) (delegateExecution.getVariable("contract_id")));
+		contract.setReleased(true);
+	}
 
 
 }
